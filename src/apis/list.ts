de27a8ecb891a2cddcf25d1588897352b21d74a7
resurgam <11110352+resurgam@user.noreply.gitee.com>
2024-11-22 11:50:24 +0800

    测试接口

--- src/apis/list.ts
+++ src/apis/list.ts
@@ -1,1 +1,10 @@
 import { http } from "@/utils";
+import type { ResType } from "./shared";
+type ChannelRes = {
+    channels: { id: number; name: string}[]
+}
+export function fetchChannelAPI(){
+    return http.request<ResType<ChannelRes>>({
+        url: '/channels'
+    })
+}
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,4 +1,11 @@
+import { fetchChannelAPI } from "@/apis/list"
+import { useEffect } from "react"
 const List = ()=>{
+    useEffect(()=>{
+       fetchChannelAPI().then((res)=>{
+        console.log(res.data.data.channels, '+++++')
+       }) 
+    }, [])
     return <div>this is List</div>
 }
 export default List
